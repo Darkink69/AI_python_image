62ddcb198719965a3311897ca9d201103843040d
Vladimir Arkatov <78140134+Darkink69@users.noreply.github.com>
2022-06-17 10:05:32 +0700

    Update clc_depo.py

--- clc_depo.py
+++ clc_depo.py
@@ -1,8 +1,8 @@
 import pyautogui
 
-f = open('output.txt')
-for line in f:
-    print(line)
+# f = open('output.txt')
+# for line in f:
+#     print(line)
 
-# for i in range(50):
-#     pyautogui.click(1480, 615, interval=1.5)
+for i in range(50):
+    pyautogui.click(1480, 615, interval=1.5)
